e965d07a734159fb25835eb3764c75951ab44643
Paul <paul@paul-mayer.eu>
2020-08-21 16:34:45 +0200

    Init

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "smelte/src/tailwind.css";
+  import TextField from "smelte/src/components/TextField";
+  import Button from "smelte/src/components/Button";
+  import ProgressCircular from "smelte/src/components/ProgressCircular";
+
+  function handleClick() {
+    console.log("test");
+  }
+</script>
+
+<main>
+  <h4 class="h4">123Karl Search</h4>
+  <TextField class="col-span-3 ..." label="Type a movie and a year: " />
+  <Button on:click={handleClick}>Search</Button>
+  <ProgressCircular />
+</main>
